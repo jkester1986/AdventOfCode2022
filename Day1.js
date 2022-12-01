b56ea109afd1b89736d9c0671cf9d16b4e31ec04
Julia Kester <kester@oswego.edu>
2022-12-01 18:54:32 -0500

    comment

--- Day1.js
+++ Day1.js
@@ -7,12 +7,16 @@ fs.readFile('Day1.txt', 'utf8', function (err, data) {
 	let highest = 0;
 	let secondHighest = 0;
 	let thirdHighest = 0;
-	
+
 	elves.forEach((elf, i) => {
 		let calories = elf.split('\n').map(Number);
 		let total = calories.reduce((partialSum, a) => partialSum + a, 0);
 
-
+        /**
+         * Yes, I realize now I could have just sorted the stupid totals.
+         * Why didn't I do that originally? I have no excuse, really,
+         * other than that it was midnight and I haven't done these puzzles in a year
+         */
 		if (total > highest) {
 			thirdHighest = secondHighest;
 			secondHighest = highest;
