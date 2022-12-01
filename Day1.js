3c59ccea2faf20fa09fdffd34d7b025b3e7f1e4c
Julia Kester <kester@oswego.edu>
2022-12-01 00:12:47 -0500

    Day 1 complete

--- Day1.js
+++ Day1.js
@@ -3,7 +3,29 @@ fs.readFile('Day1.txt', 'utf8', function (err, data) {
 	if (err) {
 		return console.log(err);
 	}
-	let lines = data.split('\n').map(Number);
-	let length = lines.length;
+	let elves = data.split('\n\n');
+	let highest = 0;
+	let secondHighest = 0;
+	let thirdHighest = 0;
+	
+	elves.forEach((elf, i) => {
+		let calories = elf.split('\n').map(Number);
+		let total = calories.reduce((partialSum, a) => partialSum + a, 0);
 
+
+		if (total > highest) {
+			thirdHighest = secondHighest;
+			secondHighest = highest;
+			highest = total;
+		}
+		else if (total > secondHighest) {
+			thirdHighest = secondHighest;
+			secondHighest = total;
+		}
+		else if (total > thirdHighest) {
+			thirdHighest = total;
+		}
+	});
+	console.log(highest);
+	console.log(highest + secondHighest + thirdHighest)
 });
