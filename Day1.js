1cc860629ad16840b7498d0c3b6441f316d7d6a1
Julia Kester <kester@oswego.edu>
2022-12-04 13:20:59 -0500

    Day 3 solved

--- Day1.js
+++ Day1.js
@@ -12,11 +12,11 @@ fs.readFile('Day1.txt', 'utf8', function (err, data) {
 		let calories = elf.split('\n').map(Number);
 		let total = calories.reduce((partialSum, a) => partialSum + a, 0);
 
-        /**
-         * Yes, I realize now I could have just sorted the stupid totals.
-         * Why didn't I do that originally? I have no excuse, really,
-         * other than that it was midnight and I haven't done these puzzles in a year
-         */
+		/**
+		 * Yes, I realize now I could have just sorted the stupid totals.
+		 * Why didn't I do that originally? I have no excuse, really,
+		 * other than that it was midnight and I haven't done these puzzles in a year
+		 */
 		if (total > highest) {
 			thirdHighest = secondHighest;
 			secondHighest = highest;
---
+++ Day3.js
@@ -0,0 +1,37 @@
+fs = require('fs');
+fs.readFile('Day3.txt', 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    let lines = data.split('\n');
+    const length = lines.length;
+    const dictionary = {}
+    let priorityTotal = 0;
+    let badgePriorities = 0;
+
+    // create dictionary of a-zA-Z with points 1-52
+    for (i = 0; i < 52; i++) {
+        letter = ((i % 26) + 10).toString(36);
+        dictionary[i > 25 ? letter.toUpperCase() : letter] = i + 1;
+    }
+
+    lines.forEach(line => {
+        const half = line.length / 2;
+        const firstHalf = line.substring(0, half).split("");
+        const secondHalf = line.substring(half).split("");
+
+        let intersection = firstHalf.find(char => secondHalf.includes(char));
+        priorityTotal += dictionary[intersection];
+    })
+
+    console.log("P1", priorityTotal);
+
+    for(i = 0; i <= length-3; i += 3) {
+        let intersection1 = lines[i].split("").filter(char => lines[i+1].includes(char));
+        let intersection2 = intersection1.filter(char => lines[i+2].includes(char));
+        console.log({intersection1, intersection2, thirdLine: lines[i+2]})
+        badgePriorities += dictionary[intersection2[0]]
+    }
+
+    console.log("P2", badgePriorities)
+});
