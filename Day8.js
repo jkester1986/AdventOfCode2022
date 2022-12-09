c653591d3289ddce8e3ac4c47fd383c82c52cbec
Julia Kester <kester@oswego.edu>
2022-12-08 20:32:12 -0500

    Day 8 complete

--- Day8.js
+++ Day8.js
@@ -6,44 +6,72 @@ fs.readFile('Day8.txt', 'utf8', function (err, data) {
   let treesRow = data.split('\n').map(currentRowIndex => currentRowIndex.split('').map(Number));;
   const length = treesRow.length;
   let visibleTrees = 0;
+  let bestViewingDistance = 0;
 
   treesRow.forEach((trees, currentRowIndex) => {
     const rowLength = trees.length;
+
+    // for every tree
     for(let col = 0; col < rowLength; col++ ){
       const treeHeight = trees[col];
-      const left = col === 0 ? [] : trees.slice(0, col-1);
+      const left = col === 0 ? [] : trees.slice(0, col).reverse();
       const right = col === rowLength - 1 ? [] : trees.slice(col+1, rowLength);
       let hasClearSight = false;
-      // console.log(`currentRowIndex:${currentRowIndex}, col:${col}`)
+      let viewingDistance = 1;
 
-      if (!left.some(height => height >= treeHeight) || !right.some(height => height >= treeHeight)) hasClearSight   = true;
+      // if we're in the first or last row, the tree is always visible
+      if (currentRowIndex === 0 || currentRowIndex === length - 1) {
+        viewingDistance = 0;
+        hasClearSight = true;
+      }
+      // see if any trees are greater than or equal to the current tree on the left or right; if one or both don't, set hasClearSight to true
+      if (!left.some(height => height >= treeHeight) || !right.some(height => height >= treeHeight)) hasClearSight = true;
+      let leftViewingDistance = left.findIndex(height => height >= treeHeight);
+      leftViewingDistance = leftViewingDistance === -1 ? left.length : leftViewingDistance + 1;
+      let rightViewingDistance = right.findIndex(height => height >= treeHeight);
+      rightViewingDistance = rightViewingDistance === -1 ? right.length : rightViewingDistance + 1;
+      viewingDistance = viewingDistance * leftViewingDistance * rightViewingDistance;
 
       if(currentRowIndex > 0) {
-        for (let otherRow = 0; otherRow < currentRowIndex; otherRow++) {
-          // console.log("above", `otherRow:${otherRow}, col:${col}`)
+        let distance = 0;
+        // all rows below currentRowIndex
+        for (let otherRow = currentRowIndex - 1; otherRow >= 0; otherRow--) {
+          distance++
           if (treesRow[otherRow][col] >= treeHeight) {
-            // console.log(treesRow[otherRow][col], "is too high for ", treeHeight)
+            viewingDistance *= distance;
             break;
           }
-          if (otherRow === currentRowIndex - 1) hasClearSight = true;
+          // if we've gotten to the end and no trees are too high
+          if (otherRow === 0) {
+            hasClearSight = true;
+            viewingDistance *= distance;
+          }
         }
       }
+
       if(currentRowIndex < length - 1) {
+        let distance = 0;
+        // all rows above currentRowIndex
         for (let otherRow = currentRowIndex + 1; otherRow < length; otherRow++) {
-          // console.log("below", `otherRow:${otherRow}, col:${col}`)
+          distance++
           if (treesRow[otherRow][col] >= treeHeight) {
-            // console.log(treesRow[otherRow][col], "is too high for ", treeHeight)
+            viewingDistance *= distance;
             break;
           }
-          if (otherRow === rowLength - 1) hasClearSight = true;
+          // if we've gotten to the end and no trees are too high
+          if (otherRow === rowLength - 1) {
+            hasClearSight = true;
+            viewingDistance *= distance;
+          }
         }
       }
+
       if (hasClearSight)
         visibleTrees++;
-      // console.log("")
+      if (viewingDistance > bestViewingDistance)
+        bestViewingDistance = viewingDistance;
     }
-    // console.log("----------------")
   })
 
-  console.log({visibleTrees})
+  console.log({visibleTrees, bestViewingDistance})
 });
