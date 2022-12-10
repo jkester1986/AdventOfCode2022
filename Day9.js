0b7564d0b2e2f3ce995d4478bba8bf8129826324
Julia Kester <kester@oswego.edu>
2022-12-10 13:34:12 -0500

    Day 9: refactor, needs rework for P2

--- Day9.js
+++ Day9.js
@@ -6,124 +6,113 @@ fs.readFile('Day9.txt', 'utf8', function (err, data) {
 	let lines = data.split('\n');
     const tailVisited = new Set();
     tailVisited.add("0,0");
-    let headPos = { x: 0, y: 0};
-    let tailPos = { x: 0, y: 0};
+    let knots = new Array(2).fill().map(() => ({x: 0, y: 0}))
 
 
     lines.forEach(line => {
-        // console.log("head", `${headPos.x},${headPos.y}`)
+        // console.log("head", `${head.x},${head.y}`)
 
-        // console.log("tail", `${tailPos.x},${tailPos.y}`)
-
+        // console.log("tail", `${nextKnot.x},${nextKnot.y}`)
+        let head = knots[0];
+        let nextKnot = knots[1];
+        // move initial head
         const move = line.split(" ");
         const spaces = Number(move[1]);
         const direction = move[0]
         switch(direction) {
             case "U":
-                headPos.y += spaces;
+                head.y += spaces;
                 break;
             case "D":
-                headPos.y -= spaces;
+                head.y -= spaces;
                 break;
             case "L":
-                headPos.x -= spaces;
+                head.x -= spaces;
                 break;
             case "R":
-                headPos.x += spaces;
+                head.x += spaces;
                 break;
         }
 
-        const xDiff = headPos.x - tailPos.x;
-        const yDiff = headPos.y - tailPos.y;
-        // move tail
-        if (Math.abs(xDiff) > 1 || Math.abs(yDiff) > 1) {
-            // console.log("need to move tail, head is at", `${headPos.x},${headPos.y}`)
-            // head and tail in same column or same row
-            if(headPos.x === tailPos.x || headPos.y === tailPos.y) {
-                moveTail(direction);
+        const xDiff = head.x - nextKnot.x;
+        const yDiff = head.y - nextKnot.y;
+        // move knots
+        for(let i = 0; i < knots.length - 1; i++) {
+            nextKnot = knots[i+1];
+            console.log({head, nextKnot})
+            if (Math.abs(xDiff) > 1 || Math.abs(yDiff) > 1) {
+                // head and tail in same column or same row
+                if(head.x === nextKnot.x || head.y === nextKnot.y) {
+                    moveTail(direction, head, nextKnot);
+                }
+
+                // we're diagonal
+                else {
+                    // tail lower left
+                    if (xDiff > 0 && yDiff > 0) {
+                        nextKnot.y++;
+                        nextKnot.x++;
+                    }
+                    // tail upper left
+                    else if (xDiff > 0 && yDiff < 0) {
+                        nextKnot.x++;
+                        nextKnot.y--;
+                    }
+                    // tail upper right
+                    else if (xDiff < 0 && yDiff < 0) {
+                        nextKnot.x--;
+                        nextKnot.y--;
+                    }
+                    // tail lower right
+                    else if (xDiff < 0 && yDiff > 0) {
+                        nextKnot.x--;
+                        nextKnot.y++;
+                    }
+
+                    tailVisited.add(`${nextKnot.x},${nextKnot.y}`);
+
+                    const newXDiff = head.x - nextKnot.x;
+                    const newYDiff = head.y - nextKnot.y;
+                    if (Math.abs(newXDiff) > 1 || Math.abs(newYDiff) > 1)
+                        moveTail(direction, head, nextKnot);
+                }
             }
-
-            // we're diagonal
-            else {
-                // tail lower left
-                if (xDiff > 0 && yDiff > 0) {
-                    tailPos = {
-                        y: tailPos.y + 1,
-                        x: tailPos.x + 1
-                    }
-                }
-                // tail upper left
-                else if (xDiff > 0 && yDiff < 0) {
-                    tailPos = {
-                        y: tailPos.y - 1,
-                        x: tailPos.x + 1
-                    }
-                }
-                // tail upper right
-                else if (xDiff < 0 && yDiff < 0) {
-                    tailPos = {
-                        y: tailPos.y - 1,
-                        x: tailPos.x - 1
-                    }
-                }
-                // tail lower right
-                else if (xDiff < 0 && yDiff > 0) {
-                    tailPos = {
-                        y: tailPos.y + 1,
-                        x: tailPos.x - 1
-                    }
-                }
-
-                tailVisited.add(`${tailPos.x},${tailPos.y}`);
-
-                const newXDiff = headPos.x - tailPos.x;
-                const newYDiff = headPos.y - tailPos.y;
-                if (Math.abs(newXDiff) > 1 || Math.abs(newYDiff) > 1)
-                    moveTail(direction);
-            }
+            head = nextKnot;
+            console.log("new knot location:", nextKnot)
         }
-        // console.log("----")
+        console.log({knots})
+        console.log("----------")
+
+        // process.exit();
     });
 
-    function moveTail(direction) {
-        const tailStartY = tailPos.y;
-        const tailStartX = tailPos.x;
+    function moveTail(direction, head, nextKnot) {
+        const tailStartY = nextKnot.y;
+        const tailStartX = nextKnot.x;
 
         switch(direction) {
             case "U":
-                for (let i = tailStartY; i < headPos.y - 1; i++) {
-                    tailPos = {
-                        y: tailPos.y + 1,
-                        x: tailPos.x
-                    }
-                    tailVisited.add(`${tailPos.x},${tailPos.y}`);
+                for (let i = tailStartY; i < head.y - 1; i++) {
+                    nextKnot.y++;
+                    tailVisited.add(`${nextKnot.x},${nextKnot.y}`);
                 }
                 break;
             case "D":
-                for (let i = tailStartY; i > headPos.y + 1; i--) {
-                    tailPos = {
-                        y: tailPos.y - 1,
-                        x: tailPos.x
-                    }
-                    tailVisited.add(`${tailPos.x},${tailPos.y}`);
+                for (let i = tailStartY; i > head.y + 1; i--) {
+                    nextKnot.y--;
+                    tailVisited.add(`${nextKnot.x},${nextKnot.y}`);
                 }
                 break;
             case "L":
-                for (let i = tailStartX; i > headPos.x + 1; i--) {
-                    tailPos = {
-                        y: tailPos.y,
-                        x: tailPos.x - 1
-                    }
-                    tailVisited.add(`${tailPos.x},${tailPos.y}`);
+                for (let i = tailStartX; i > head.x + 1; i--) {
+                    nextKnot.x--;
+                    tailVisited.add(`${nextKnot.x},${nextKnot.y}`);
                 }
                 break;
             case "R":
-                for (let i = tailStartX; i < headPos.x - 1; i++) {
-                    tailPos = {
-                        y: tailPos.y,
-                        x: tailPos.x + 1
-                    }
-                    tailVisited.add(`${tailPos.x},${tailPos.y}`);
+                for (let i = tailStartX; i < head.x - 1; i++) {
+                    nextKnot.x++;
+                    tailVisited.add(`${nextKnot.x},${nextKnot.y}`);
                 }
                 break;
         }
