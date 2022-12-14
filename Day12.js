91b9db1a01f5cbb7afc22a867a049625e317897f
Julia Kester <jkester@acvauctions.com>
2022-12-14 12:01:07 -0500

    Day 12 solved

--- Day12.js
+++ Day12.js
@@ -1,15 +1,11 @@
-// TODO: figure out how to have P2 run faster
 fs = require('fs');
 fs.readFile('Day12.txt', 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
   let lines = data.split('\n');
-  let height = lines.length;
-  let width = lines[0].length;
   let map = {}
   let SPos = "";
-  let endingPos = "";
   const allAs = []
 
   lines.forEach((line, y) => {
@@ -17,12 +13,10 @@ fs.readFile('Day12.txt', 'utf8', function (err, data) {
     chars.forEach((char, x) => {
       console.log.l
       const pos = `${x},${y}`;
-      if (char === "S")
+      if (char === "E")
         SPos = pos
-      else if (char === "E")
-        endingPos = pos
       map[pos] = {
-        val: char === "E" ? 26 : char.charCodeAt(0) - 97,
+        val: char === "E" ? 26 : char === "S" ? 0 : char.charCodeAt(0) - 97,
         char,
         x,
         y
@@ -33,7 +27,7 @@ fs.readFile('Day12.txt', 'utf8', function (err, data) {
   });
 
 
-  function leastSteps(startingPos, additionalPruning) {
+  function leastSteps(startingPos, findAs) {
     const visited = new Set();
     const locationQueue = [];
   
@@ -48,37 +42,20 @@ fs.readFile('Day12.txt', 'utf8', function (err, data) {
     while (locationQueue.length) {
       // find all possible next paths
       currentStep = locationQueue.shift();
-      // console.log({ steps, stepSet: currentStep, steps });
       let allNewOptions = [];
-      // console.log the grid
-      for (let y = 0; y < height; y++) {
-        let row = ""
-        for (let x = 0; x < width; x++) {
-          if (visited.has(`${x},${y}`))
-            row += "#"
-          else
-            row += "."
-        }
-        // console.log(row)
-      }
       currentStep.forEach(option => {
         let {pos} = option;
-        // if (steps === 3) process.exit();
         
         let {x, y, val, char} = map[pos];
-        // console.log({pos, char, locationQueue})
-        if (char === "E") {
+        if (char === "S" || findAs && char === "a") {
           found = true;
           return;
-          // console.log("least number of steps:", steps)
-          // process.exit();
         }
+
         // up
         const upPos = `${x},${y - 1}`;
-        // console.log({upPos})
         const upLoc = map[upPos];
-        if (upLoc && ((upLoc.val - val) <= 1 || ["S"].includes(char)) && !visited.has(upPos) && (!additionalPruning || !(allAs.includes(upPos)))) {
-          // console.log({upLoc})
+        if (upLoc && ((val - upLoc.val) <= 1 ) && !visited.has(upPos)) {
           allNewOptions.push({
             pos: upPos,
           });
@@ -87,10 +64,8 @@ fs.readFile('Day12.txt', 'utf8', function (err, data) {
     
         // down
         const downPos = `${x},${y + 1}`;
-        // console.log({downPos});
         const downLoc = map[downPos];
-        if (downLoc && ((downLoc.val - val) <= 1 || ["S"].includes(char)) && !visited.has(downPos) && (!additionalPruning || !(allAs.includes(downPos)))) {
-          // console.log({ downLoc })
+        if (downLoc && ((val - downLoc.val) <= 1 ) && !visited.has(downPos)) {
           allNewOptions.push({
             pos: downPos,
           });
@@ -99,10 +74,8 @@ fs.readFile('Day12.txt', 'utf8', function (err, data) {
     
         // left
         const leftPos = `${x - 1},${y}`;
-        // console.log({leftPos})
         const leftLoc = map[leftPos];
-        if (leftLoc && ((leftLoc.val - val) <= 1 || ["S"].includes(char)) && !visited.has(leftPos) && (!additionalPruning || !(allAs.includes(leftPos)))) {
-          // console.log({ leftLoc })
+        if (leftLoc && ((val - leftLoc.val) <= 1 ) && !visited.has(leftPos)) {
           allNewOptions.push({
             pos: leftPos,
           });
@@ -111,10 +84,8 @@ fs.readFile('Day12.txt', 'utf8', function (err, data) {
     
         // right
         const rightPos = `${x + 1},${y}`;
-        // console.log({rightPos})
         const rightLoc = map[rightPos];
-        if (rightLoc && ((rightLoc.val - val) <= 1 || ["S"].includes(char)) && !visited.has(rightPos) && (!additionalPruning || !(allAs.includes(rightPos)))) {
-          // console.log({ rightLoc })
+        if (rightLoc && ((val - rightLoc.val) <= 1  ) && !visited.has(rightPos)) {
           allNewOptions.push({
             pos: rightPos,
           });
@@ -124,24 +95,14 @@ fs.readFile('Day12.txt', 'utf8', function (err, data) {
       });
       if (found)
         return steps;
-      // console.log({allNewOptions})
-      // console.log("===========")
       locationQueue.push(allNewOptions);
-      // if (steps === 12) process.exit();
       steps++;
       if (steps > 500) return 500
     }
   }
 
   console.log("P1", leastSteps(SPos));
-
-  let lowest = 500;
-  allAs.forEach(a => {
-    const stepCount = leastSteps(a, true);
-    if (stepCount < lowest) lowest = stepCount;
-  });
-
-  console.log("P2", lowest)
+  console.log("P2", leastSteps(SPos, true));
 
 
 });
